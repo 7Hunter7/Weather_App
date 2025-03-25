31627549172cbc9b220a57dbea627333378f1430
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-25 07:35:17 +0300

    Updated 'App.vue' - Added div with class 'wrapper'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup></script>
 
 <template>
-  <h1>Hello!</h1>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, commodi iusto dolores modi maxime
-    possimus nisi enim reiciendis quidem neque nemo nihil eum labore ad quas! Consequuntur corporis
-    voluptate eius!
-  </p>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в вашем городе</p>
+  </div>
 </template>
 
 <style scoped></style>

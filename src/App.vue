35573599285e14a751c9368c76f1fd3cef22e979
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-25 07:45:24 +0300

    Updated 'App.vue' - Updated  styles for 'wrapper' with lang 'sass' for CSS

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,20 @@
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .wrapper {
   width: 900px;
   height: 500px;
   border-radius: 50px;
+  padding: 20px;
   background-color: #1f0f24;
+  text-align: center;
+  color: rgb(40, 200, 150);
+  & h1 {
+    margin-top: 50px;
+  }
+  & p {
+    margin-top: 20px;
+  }
 }
 </style>

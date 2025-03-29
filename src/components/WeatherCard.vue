31a3dc090e93991ac4c1cdf92ca991b0f4953cdd
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-29 17:33:05 +0300

    Updated component 'WeatherCard' -  Updated template with using 'wind.gust', 'wind.deg', 'rain' and 'grnd_level'. Added computeds 'rainUnit', 'grndLevelUnit', 'groundLevelPressureMmHg' and 'windDirection'

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -7,11 +7,21 @@
       Ощущается как: {{ weather.main.feels_like.toFixed(0) }}{{ temperatureUnit }}
     </p>
     <p v-if="weather.main?.temp_max">
-      Максимальная температура: {{ weather.main.temp_max.toFixed(0) }}{{ temperatureUnit }}
+      Максимальная температура (сейчас): {{ weather.main.temp_max.toFixed(0) }}{{ temperatureUnit }}
     </p>
     <p v-if="weather.main?.humidity">Влажность воздуха: {{ weather.main.humidity.toFixed(0) }}%</p>
     <p v-if="weather.wind?.speed">
-      Скорость ветра до: {{ weather.wind.speed.toFixed(1) }} {{ windSpeedUnit }}
+      Скорость ветра: {{ weather.wind.speed.toFixed(1) }} {{ windSpeedUnit }}
+    </p>
+    <p v-if="weather.wind?.gust">
+      Порывы ветра: до {{ weather.wind.gust.toFixed(1) }} {{ windSpeedUnit }}
+    </p>
+    <p v-if="weather.wind?.deg">Направление ветра: {{ windDirection }}</p>
+    <p v-if="weather.rain?.['1h']">
+      Количество осадков(за 1 час): {{ weather.rain['1h'].toFixed(1) }}{{ rainUnit }}
+    </p>
+    <p v-if="weather.main?.grnd_level">
+      Атмосферное давление: {{ groundLevelPressureMmHg }} {{ grndLevelUnit }}
     </p>
     <p v-if="weather.weather?.[0]?.description">
       Погодные условия: {{ weather.weather[0].description }}
@@ -36,6 +46,34 @@ const props = defineProps({
 
 const temperatureUnit = computed(() => (props.units === 'metric' ? '℃' : '℉'))
 const windSpeedUnit = computed(() => (props.units === 'metric' ? 'м/с' : 'mph'))
+const rainUnit = computed(() => (props.units === 'metric' ? 'мм/ч' : 'mm/h'))
+const grndLevelUnit = computed(() => (props.units === 'metric' ? 'мм рт.ст.' : 'hPa'))
+
+// Атмосферное давление
+const groundLevelPressureMmHg = computed(() => {
+  const grndLevel = props.weather.main?.grnd_level
+  if (grndLevel) {
+    let pressureHPa = null
+    if (props.units === 'imperial') {
+      pressureHPa = grndLevel // Давление в гектопаскалях
+    } else {
+      pressureHPa = (grndLevel * 0.750062).toFixed(1) // Давление в мм рт. ст.
+    }
+    return pressureHPa
+  }
+  return null
+})
+
+// Направление ветра
+const windDirection = computed(() => {
+  if (props.weather.wind?.deg) {
+    const degrees = props.weather.wind.deg
+    const directions = ['С', 'СВ', 'В', 'ЮВ', 'Ю', 'ЮЗ', 'З', 'СЗ']
+    const index = Math.round(degrees / 45) % 8
+    return directions[index]
+  }
+  return null
+})
 </script>
 
 <style scoped lang="scss">

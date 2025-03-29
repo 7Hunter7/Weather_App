c635ba7efc190f15ce8fb6fb6124f1348207537b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-29 17:42:04 +0300

    Created new file 'i18n.js' - Configuring the vue-i18n

---
+++ src/i18n.js
@@ -0,0 +1,47 @@
+// Изменение языка интерфейса
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+  en: {
+    appTitle: 'Weather App',
+    cityPlaceholder: 'Enter city name',
+    getWeather: 'Get Weather',
+    detectLocation: 'Show weather in my city',
+    update: 'Update',
+    temperature: 'Temperature',
+    feelsLike: 'Feels like',
+    maxTemperature: 'Max Temperature',
+    humidity: 'Humidity',
+    windSpeed: 'Wind Speed',
+    windGust: 'Wind Gust',
+    windDirection: 'Wind Direction',
+    precipitation: 'Precipitation(last 1 hour)',
+    groundPressure: 'Level Pressure',
+    weatherConditions: 'Weather Conditions',
+  },
+  ru: {
+    appTitle: 'Погодное приложение',
+    cityPlaceholder: 'Введите название города',
+    getWeather: 'Узнать погоду',
+    detectLocation: 'Показать погоду в моём городе',
+    update: 'Обновить',
+    temperature: 'Температура',
+    feelsLike: 'Ощущается как',
+    maxTemperature: 'Максимальная температура',
+    humidity: 'Влажность воздуха',
+    windSpeed: 'Скорость ветра',
+    windGust: 'Порывы ветра',
+    windDirection: 'Направление ветра',
+    precipitation: 'Количество осадков(за 1 час)',
+    groundPressure: 'Атмосферное давление',
+    weatherConditions: 'Погодные условия',
+  },
+}
+
+const i18n = createI18n({
+  locale: 'ru', // Значение по умолчанию
+  fallbackLocale: 'en', // Язык, используемый, если перевод отсутствует
+  messages,
+})
+
+export default i18n

5ec979bfc4b5880d6e8dc346eb6c0fd43caeae30
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-02 10:41:47 +0300

    Created new component 'InputWithValidation.vue' - Contains <input> that is bound to the modelValue using v-model and listens to the input, keydown.enter, and keydown.esc events. It displays error message if ErrorMessage is not an empty string

---
+++ src/components/InputWithValidation.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input
+      class="input"
+      type="text"
+      :value="modelValue"
+      :placeholder="$t('cityPlaceholder')"
+      @input="$emit('update:modelValue', $event.target.value)"
+      @keydown.enter="$emit('get-weather')"
+      @keydown.esc="$emit('clear-input')"
+    />
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  errorMessage: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:modelValue', 'get-weather', 'clear-input'])
+</script>
+
+<style scoped lang="scss">
+.input {
+  padding: var(--indents-little) var(--indents-small);
+  border: none;
+  outline: none; /* Удаляем обводку у input */
+  border-bottom: 2px solid var(--border-bottom-color);
+  background: transparent;
+  color: var(--input-color);
+  font-size: var(--font-size-input);
+
+  &:focus {
+    border-bottom-color: var(--loading-color);
+  }
+}
+
+.error {
+  margin-top: var(--indents-medium);
+  color: var(--error-color);
+}
+</style>

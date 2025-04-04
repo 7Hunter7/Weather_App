e8089b037e60e2c1a08986a26b153eed33c7f1a1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-04 12:23:11 +0300

    Created new component 'HelpDialog.vue'

---
+++ src/components/HelpDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="help-dialog" @click.self="$emit('close')">
+    <div class="help-dialog__content">
+      <h2>{{ $t('helpTitle') }}</h2>
+      <slot />
+      <button @click="$emit('close')">{{ $t('close') }}</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineEmits(['close'])
+</script>
+
+<style scoped lang="scss">
+.help-dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /*  Полупрозрачный фон */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+
+  &__content {
+    background-color: var(--white-color);
+    padding: var(--indents-medium);
+    border-radius: var(--border-radius-small);
+    width: 80%;
+    max-width: var(--wrapper-min-width);
+    position: relative;
+  }
+}
+</style>
